081a034cdcc85c6065be377e66d135db62a99ce4
Erasylbruh <igrok.20062003@gmail.com>
2025-12-02 22:20:10 +0500

    vercelfix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Chat from './pages/Chat';
+import NotFound from './pages/NotFound';
 import './index.css';
 
 import { LanguageProvider } from './context/LanguageContext';
@@ -10,12 +14,17 @@ function App() {
   return (
     <LanguageProvider>
       <Router>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+        <div className="app flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
         </div>
       </Router>
     </LanguageProvider>
